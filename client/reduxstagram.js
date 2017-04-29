778a33ee24f63a9a56a86a00d159e54e9af93270
xrr2016 <xiaoranran1993@163.com>
2017-04-29 22:45:24 +0800

    :zap:

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import css from './styles/style.styl'
+
+const app = document.getElementById('app')
+render(
+  <h1>Hello</h1>,
+  app
+)
