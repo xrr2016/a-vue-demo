32c52e8625cc8353d281712a527ed34d9d79208d
xrr2016 <xiaoranran1993@163.com>
2017-05-02 23:19:42 +0800

    :heavy_plus_sign:

--- client/store/index.js
+++ client/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux'
+import { createStore, compose } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
@@ -11,9 +11,21 @@ const defaultState = {
   comments
 }
 
-const store = createStore(rootReducer, defaultState)
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
+const store = createStore(rootReducer, defaultState, enhancers)
 
 export const history = syncHistoryWithStore(browserHistory, store)
+
+if (module.hot) {
+  module.hot.accept('../reducers/', () => {
+    const nextReducers = require('../reducers/index').default
+    store.replaceReducer(nextReducers)
+  })
+}
+
 export default store
 
 
